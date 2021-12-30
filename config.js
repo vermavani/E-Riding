e6c94c2a481018915b7f532640f3c881c621f955
vermavani <78356799+vermavani@users.noreply.github.com>
2021-12-30 10:10:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyC8ctQxCf4JnCqOAG-toLi03GjyUkRLyv4",
+    authDomain: "e-riding.firebaseapp.com",
+    projectId: "e-riding",
+    storageBucket: "e-riding.appspot.com",
+    messagingSenderId: "565335839257",
+    appId: "1:565335839257:web:efa4ed3f17ada3b3025088"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
